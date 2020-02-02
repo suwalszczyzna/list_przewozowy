b4b7a26615cb3ca829de4f0f0484b8914f2247e3
Damian Suwała <damian.suwala@gmail.com>
2020-02-02 20:40:54 +0100

    v1

--- ListPrzewozowy.py
+++ ListPrzewozowy.py
@@ -8,28 +8,46 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+from PyQt5.QtWidgets import QFileDialog
 
 
 class MainWindow(object):
     def __init__(self, MainWindow):
+
+        self.filePath = ''
+
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(623, 372)
-        MainWindow.setMaximumSize(QtCore.QSize(623, 372))
+        MainWindow.resize(713, 788)
+        MainWindow.setMaximumSize(QtCore.QSize(2000, 2000))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-
-        self.openFileButton = QtWidgets.QPushButton(self.centralwidget)
-        self.openFileButton.setGeometry(QtCore.QRect(20, 20, 101, 31))
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 10, 701, 711))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(5, 5, 5, 5)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setContentsMargins(-1, 10, -1, 10)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.openFileButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.openFileButton.setMinimumSize(QtCore.QSize(0, 40))
+        self.openFileButton.setMaximumSize(QtCore.QSize(120, 100))
         self.openFileButton.setObjectName("openFileButton")
-
-        self.filePathLabel = QtWidgets.QLabel(self.centralwidget)
-        self.filePathLabel.setGeometry(QtCore.QRect(130, 20, 171, 31))
+        self.horizontalLayout.addWidget(self.openFileButton)
+        self.filePathLabel = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.filePathLabel.setObjectName("filePathLabel")
-
-        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(20, 70, 581, 192))
-        self.tableWidget.setMinimumSize(QtCore.QSize(571, 0))
-
+        self.horizontalLayout.addWidget(self.filePathLabel)
+        self.addParcelManButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.addParcelManButton.setMinimumSize(QtCore.QSize(0, 40))
+        self.addParcelManButton.setMaximumSize(QtCore.QSize(120, 40))
+        self.addParcelManButton.setObjectName("addParcelManButton")
+        self.horizontalLayout.addWidget(self.addParcelManButton)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.tableWidget = QtWidgets.QTableWidget(self.verticalLayoutWidget)
+        self.tableWidget.setMinimumSize(QtCore.QSize(570, 0))
         palette = QtGui.QPalette()
         brush = QtGui.QBrush(QtGui.QColor(0, 157, 255))
         brush.setStyle(QtCore.Qt.SolidPattern)
@@ -40,22 +58,28 @@ class MainWindow(object):
         brush = QtGui.QBrush(QtGui.QColor(0, 157, 255))
         brush.setStyle(QtCore.Qt.SolidPattern)
         palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.AlternateBase, brush)
-
         self.tableWidget.setPalette(palette)
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        self.tableWidget.setFont(font)
+        self.tableWidget.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.tableWidget.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
         self.tableWidget.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustIgnored)
+        self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContentsOnFirstShow)
         self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.AllEditTriggers)
+        self.tableWidget.setProperty("showDropIndicator", False)
         self.tableWidget.setDragEnabled(True)
-        self.tableWidget.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
-        self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+        self.tableWidget.setDragDropMode(QtWidgets.QAbstractItemView.NoDragDrop)
+        self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
         self.tableWidget.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.tableWidget.setIconSize(QtCore.QSize(10, 10))
         self.tableWidget.setShowGrid(True)
         self.tableWidget.setGridStyle(QtCore.Qt.SolidLine)
         self.tableWidget.setWordWrap(False)
+        self.tableWidget.setCornerButtonEnabled(False)
+        self.tableWidget.setRowCount(0)
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(5)
-
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(0, item)
         item = QtWidgets.QTableWidgetItem()
@@ -66,64 +90,84 @@ class MainWindow(object):
         self.tableWidget.setHorizontalHeaderItem(3, item)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(4, item)
-
         self.tableWidget.horizontalHeader().setCascadingSectionResizes(True)
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(150)
+        self.tableWidget.horizontalHeader().setMinimumSectionSize(50)
         self.tableWidget.horizontalHeader().setSortIndicatorShown(True)
         self.tableWidget.horizontalHeader().setStretchLastSection(True)
         self.tableWidget.verticalHeader().setVisible(True)
         self.tableWidget.verticalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.verticalHeader().setDefaultSectionSize(30)
         self.tableWidget.verticalHeader().setSortIndicatorShown(False)
         self.tableWidget.verticalHeader().setStretchLastSection(False)
-
-        self.addParcelManButton = QtWidgets.QPushButton(self.centralwidget)
-        self.addParcelManButton.setGeometry(QtCore.QRect(500, 20, 101, 31))
-        self.addParcelManButton.setObjectName("addParcelManButton")
-
-        self.sendParcelsButton = QtWidgets.QPushButton(self.centralwidget)
-        self.sendParcelsButton.setGeometry(QtCore.QRect(500, 280, 101, 31))
-        self.sendParcelsButton.setObjectName("sendParcelsButton")
-
-        self.deleteSelectedParcelsButton = QtWidgets.QPushButton(self.centralwidget)
-        self.deleteSelectedParcelsButton.setGeometry(QtCore.QRect(20, 280, 121, 31))
-        self.deleteSelectedParcelsButton.setObjectName("deleteSelectedParcelsButton")
-
-        self.deleteAllParcelsButton = QtWidgets.QPushButton(self.centralwidget)
-        self.deleteAllParcelsButton.setGeometry(QtCore.QRect(150, 280, 121, 31))
+        self.verticalLayout.addWidget(self.tableWidget)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setContentsMargins(-1, 10, -1, 10)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.deleteAllParcelsButton_2 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.deleteAllParcelsButton_2.setObjectName("deleteAllParcelsButton_2")
+        self.horizontalLayout_2.addWidget(self.deleteAllParcelsButton_2)
+        self.deleteAllParcelsButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.deleteAllParcelsButton.setObjectName("deleteAllParcelsButton")
+        self.horizontalLayout_2.addWidget(self.deleteAllParcelsButton)
+        self.deleteSelectedParcelsButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.deleteSelectedParcelsButton.setObjectName("deleteSelectedParcelsButton")
+        self.horizontalLayout_2.addWidget(self.deleteSelectedParcelsButton)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setContentsMargins(5, 5, 5, 5)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem1)
+        self.sendParcelsButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.sendParcelsButton.setMinimumSize(QtCore.QSize(0, 50))
+        self.sendParcelsButton.setMaximumSize(QtCore.QSize(300, 16777215))
+        self.sendParcelsButton.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.sendParcelsButton.setAutoFillBackground(True)
+        self.sendParcelsButton.setFlat(False)
+        self.sendParcelsButton.setObjectName("sendParcelsButton")
+        self.horizontalLayout_3.addWidget(self.sendParcelsButton)
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
         MainWindow.setCentralWidget(self.centralwidget)
-
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 623, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 713, 26))
         self.menubar.setObjectName("menubar")
         self.menuPlik = QtWidgets.QMenu(self.menubar)
         self.menuPlik.setObjectName("menuPlik")
         MainWindow.setMenuBar(self.menubar)
-
         self.actionOpenFile = QtWidgets.QAction(MainWindow)
         self.actionOpenFile.setObjectName("actionOpenFile")
         self.actionSettings = QtWidgets.QAction(MainWindow)
         self.actionSettings.setObjectName("actionSettings")
         self.actionExit = QtWidgets.QAction(MainWindow)
         self.actionExit.setObjectName("actionExit")
-
         self.menuPlik.addAction(self.actionOpenFile)
         self.menuPlik.addAction(self.actionSettings)
         self.menuPlik.addSeparator()
         self.menuPlik.addAction(self.actionExit)
         self.menubar.addAction(self.menuPlik.menuAction())
 
+        header = self.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(4, QtWidgets.QHeaderView.ResizeToContents)
+
         self.translate_ui(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def translate_ui(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Listy przewozowe Optima -> Shoper"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.openFileButton.setText(_translate("MainWindow", "Otwórz plik..."))
         self.filePathLabel.setText(_translate("MainWindow", "Brak pliku :("))
+        self.addParcelManButton.setText(_translate("MainWindow", "Dodaj ręcznie"))
         self.tableWidget.setSortingEnabled(True)
         item = self.tableWidget.horizontalHeaderItem(1)
         item.setText(_translate("MainWindow", "Nr zam. Shopper"))
@@ -133,16 +177,21 @@ class MainWindow(object):
         item.setText(_translate("MainWindow", "Nr dok. Optima"))
         item = self.tableWidget.horizontalHeaderItem(4)
         item.setText(_translate("MainWindow", "Nazwa"))
-        self.addParcelManButton.setText(_translate("MainWindow", "Dodaj ręcznie"))
-        self.sendParcelsButton.setText(_translate("MainWindow", "Wyślij..."))
-        self.deleteSelectedParcelsButton.setText(_translate("MainWindow", "Usuń zaznaczone"))
+        self.deleteAllParcelsButton_2.setText(_translate("MainWindow", "Zaznacz wszystkie"))
         self.deleteAllParcelsButton.setText(_translate("MainWindow", "Usuń wszystkie"))
+        self.deleteSelectedParcelsButton.setText(_translate("MainWindow", "Usuń zaznaczone"))
+        self.sendParcelsButton.setText(_translate("MainWindow", "Wyślij..."))
         self.menuPlik.setTitle(_translate("MainWindow", "Plik"))
         self.actionOpenFile.setText(_translate("MainWindow", "Otwórz plik..."))
         self.actionSettings.setText(_translate("MainWindow", "Ustawienia"))
         self.actionExit.setText(_translate("MainWindow", "Zamknij program"))
 
-    def add_parcel(self):
+        self.openFileButton.clicked.connect(self.open_file_button_handler)
+
+    def open_file_button_handler(self):
+        self.open_file_name_dialog()
+
+    def add_parcel(self, shoper, parcel_no, optima_invoice, name):
         row_position = self.tableWidget.rowCount()
         self.tableWidget.insertRow(row_position)
 
@@ -156,7 +205,54 @@ class MainWindow(object):
         cell_widget.setLayout(lay_out)
 
         self.tableWidget.setCellWidget(row_position, 0, cell_widget)
-        self.tableWidget.setItem(row_position, 1, QtWidgets.QTableWidgetItem('Test'))
+        self.tableWidget.setItem(row_position, 1, QtWidgets.QTableWidgetItem(shoper))
+        self.tableWidget.setItem(row_position, 2, QtWidgets.QTableWidgetItem(parcel_no))
+        self.tableWidget.setItem(row_position, 3, QtWidgets.QTableWidgetItem(optima_invoice))
+        self.tableWidget.setItem(row_position, 4, QtWidgets.QTableWidgetItem(name))
+
+    def populate_data(self, input_data):
+        for dictionary in input_data:
+            self.add_parcel(str(dictionary.get('Opis')),
+                            str(dictionary.get('Nr listu przewozowego')),
+                            str(dictionary.get('Numer dokumentu')),
+                            str(dictionary.get('Kontrahent'))
+                            )
+
+    def read_data_from_file(self, path):
+        import pandas
+        self.filePath = path
+        self.filePathLabel.setText(self.filePath)
+        excel_df = pandas.read_excel(path,
+                                     usecols=['Nr listu przewozowego', 'Opis', 'Numer dokumentu', 'Kontrahent'])
+        data = excel_df.to_dict(orient='record')
+        if data:
+            data.pop()
+            for dictionary in data:
+                opis = str(dictionary.get('Opis'))
+                nr_listu = str(dictionary.get('Nr listu przewozowego'))
+                import re
+                regex_zamowienie = re.compile(r'Zam.wienie.(\d{5})$|(\d{5})$')
+                regex_nr_listu = re.compile(r'(\d+\w)')
+
+                zamowienie_results = re.findall(regex_zamowienie, opis)
+                if zamowienie_results:
+                    opis = zamowienie_results[0][0] if zamowienie_results[0][0] else zamowienie_results[0][1]
+                    dictionary['Opis'] = opis
+
+                numer_listu_results = re.findall(regex_nr_listu, nr_listu)
+                if numer_listu_results:
+                    dictionary['Nr listu przewozowego'] = numer_listu_results[-1]
+
+            return data
+        else:
+            return None
+
+    def open_file_name_dialog(self):
+        file_name, _ = QFileDialog.getOpenFileName()
+        if file_name:
+            print(file_name)
+            data = self.read_data_from_file(file_name)
+            self.populate_data(data)
 
 
 if __name__ == "__main__":
@@ -165,5 +261,4 @@ if __name__ == "__main__":
     window = QtWidgets.QMainWindow()
     ui = MainWindow(window)
     window.show()
-    ui.add_parcel()
     sys.exit(app.exec_())
